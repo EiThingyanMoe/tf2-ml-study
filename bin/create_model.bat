a3ad570d50c2e1694ecf1f75c883f6b2e0163625
Ei Thingyan Moe <57161225+EiThingyanMoe@users.noreply.github.com>
2023-07-10 22:17:02 +0630

    add execution files

---
+++ bin/create_model.bat
@@ -0,0 +1,3 @@
+set FLASK_ENV=development
+set SCRIPT_ENV=development
+python ..\src\flower_model_creator.py
---
+++ bin/start_app_prod.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+export FLASK_ENV=production
+export SCRIPT_ENV=production
+python ../src/app.py
